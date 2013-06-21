b764ecdd74ed7931e5e81cf4f4b98fcb78ff72e1
Tim Snowhite <tsnowhite@taximagic.com>
2013-06-21 11:09:29 -0400

    [pair-disconnects] a Connection-pair echo server, with segfaults.
    typing-for-war> PWD=~/Desktop/typing-for-war/ PORT=5002 go run web.go 
    listening...
    2013/06/21 10:24:59 http: panic serving 192.168.0.195:50144: runtime error: invalid memory address or nil pointer dereference
    /usr/local/Cellar/go/1.0.3/src/pkg/net/http/server.go:589 (0x43fec)
    	_func_004: buf.Write(debug.Stack())
    /usr/local/Cellar/go/1.0.3/src/pkg/runtime/proc.c:1443 (0x12572)
    	panic: reflect·call(d->fn, d->args, d->siz);
    /usr/local/Cellar/go/1.0.3/src/pkg/runtime/runtime.c:128 (0x1303e)
    	panicstring: runtime·panic(err);
    /usr/local/Cellar/go/1.0.3/src/pkg/runtime/thread_darwin.c:418 (0x1618d)
    	sigpanic: runtime·panicstring("invalid memory address or nil pointer dereference");
    /Users/tsnow/Desktop/typing-for-war/web.go:23 (0x201c)
    	(*GameList).Connect: if this.waiting == nil {
    /Users/tsnow/Desktop/typing-for-war/web.go:171 (0x3941)
    	_func_005: game :=	 games.Connect(sock)
    /Users/tsnow/dev/prod/goroot/src/code.google.com/p/go.net/websocket/server.go:98 (0x51639)
    	google.com/p/go.net/websocket.Server.serveWebSocket: s.Handler(conn)
    /Users/tsnow/dev/prod/goroot/src/code.google.com/p/go.net/websocket/server.go:121 (0x5179e)
    	google.com/p/go.net/websocket.Handler.ServeHTTP: s.serveWebSocket(w, req)
    /usr/local/Cellar/go/1.0.3/src/pkg/net/http/server.go:941 (0x38939)
    	(*ServeMux).ServeHTTP: mux.handler(r).ServeHTTP(w, r)
    /usr/local/Cellar/go/1.0.3/src/pkg/net/http/server.go:669 (0x378f8)
    	(*conn).serve: handler.ServeHTTP(w, w.req)
    /usr/local/Cellar/go/1.0.3/src/pkg/runtime/proc.c:271 (0x10678)
    	goexit: runtime·goexit(void)

--- web.go
+++ web.go
@@ -11,6 +11,109 @@ import (
 	"html/template"
 )
 
+    // OO UP IN THIS BITCH
+    type GameList struct {
+    	active []*Game
+    	waiting *Game
+    }
+
+    func (this *GameList) Connect(conn *ws.Conn) *Game {
+    	// if waiting is empty, create a new waiting game
+    	// if waiting has any, FIFO complete game
+    	if this.waiting == nil {
+    		game := New()
+    		game.Connect(conn)
+    		this.waiting = game
+    	} else {
+    		this.waiting.Connect(conn)
+    		this.active = append(this.active,this.waiting)
+    		this.waiting.Broadcast("A game done did begunnned.")
+    		return this.waiting
+    		//this.waiting = nil
+    	}
+    	return this.waiting
+    }
+
+	type Game struct {
+		first *ws.Conn
+		last *ws.Conn
+		disconnect chan bool
+	}
+
+	func New() (game *Game) {
+		game = new(Game)
+		game.disconnect = make(chan bool)
+		go func(){
+			for {
+				select {
+					case disconnect := <- game.disconnect:
+						if disconnect {
+							game.first.Close()
+							game.last.Close()
+							log.Print("- game ", game.first.RemoteAddr(), "<->",game.last.RemoteAddr(), " disconnected")
+						}
+				}
+			}
+		}()
+		return
+	}
+	func (this *Game) Clone() (game *Game){
+		game.first = this.first
+		game.last = this.last
+		game.disconnect = this.disconnect
+		return
+	}
+
+	func (this *Game) Connect(conn *ws.Conn) {
+		if this.first != nil {
+			log.Print("- game last ", conn.RemoteAddr(), " connected")
+			this.last = conn
+		} else {
+			log.Print("- game first ", conn.RemoteAddr(), " connected")
+			this.first = conn
+		}
+	}
+
+	func (this *Game) Disconnect() {
+		this.disconnect <- true
+	}
+
+	func (this *Game) Broadcast (message string) {
+		if ws.Message.Send(this.first,message) != nil {
+			this.Disconnect()
+		}
+		if ws.Message.Send(this.last, message) != nil {
+			this.Disconnect()
+		}
+
+	}
+
+	var games *GameList
+	// OO DONE 
+/*
+	type game struct {
+		conns []*ws.Conn
+		disconnect chan bool
+	}
+	var	current_game = &game{disconnect: make(chan bool)}
+	func disconnectOnError(current_game *game) {
+	go func(){
+		for {
+			select {
+				case disconnect := <- current_game.disconnect:
+					if disconnect {
+						for _,conn := range current_game.conns {
+							conn.Close()
+							log.Print("- game ", conn.RemoteAddr(), " disconnected")
+						}
+						current_game.conns = nil
+					}
+			}
+		}
+	}()
+	}
+*/
+
 func main() {
 /* Blood and destruction shall be so in use 
 And dreadful objects so familiar 
@@ -24,11 +127,11 @@ Cry 'Havoc,' and let slip the dogs of war;
 That this foul deed shall smell above the earth 
 With carrion men, groaning for burial. */
 
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		http.ServeFile(res,req,"/app/index.html")
+	http.HandleFunc("/app/index", func(res http.ResponseWriter, req *http.Request) {
+		http.ServeFile(res,req,"/app/index.html") // /app/index.html for heroku
 	})
-	http.Handle("/files", http.FileServer(http.Dir(os.Getenv("PWD"))))
-	http.Handle("/socket/server", ws.Handler(func(sock *ws.Conn){
+	http.Handle("/", http.FileServer(http.Dir(os.Getenv("PWD")))) 
+	http.Handle("/socket/echo", ws.Handler(func(sock *ws.Conn){
 		log.Print("- ", sock.RemoteAddr(), " connected")
 		var message string
 		if ws.Message.Receive(sock, &message) != nil {
@@ -41,6 +144,21 @@ With carrion men, groaning for burial. */
 		sock.Close()
 		log.Print("- ", sock.RemoteAddr(), " disconnected")
 	}))
+
+	// disconnectOnError(current_game)
+
+	http.Handle("/socket/new_game", ws.Handler(func(sock *ws.Conn){
+		game :=	 games.Connect(sock)
+
+		var message string
+		if ws.Message.Receive(sock, &message) != nil {
+			log.Print("- game ", sock.RemoteAddr(), " couldn't receive.")
+			game.disconnect <- true
+		}
+		game.Broadcast(message)
+	}))
+
+
 	template.New("things")
 	fmt.Println("listening...")
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
